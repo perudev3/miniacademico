cf2758757382d80618a60b33bfdf7eff63903aeb
perudev3 <peru.dev3@gmail.com>
2019-11-21 13:36:13 -0600

    Login con autenticacion Google

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::get('/{provider}', 'Auth\LoginController@redirectToProvider');
+    Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
+
+    Route::get('/{provider}', 'Auth\AuthFacebook@redirectToProvider');
+	Route::get('/{provider}/callback', 'Auth\AuthFacebook@handleProviderCallback');
+});
+
+
